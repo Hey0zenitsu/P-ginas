5cdfc8541a5143cda7ce53e88c77d01e869d4e83
hey0zenitsu <bd62593@gmail.com>
2024-04-23 20:15:08 -0300

    mudança de lista (agr ela fica oculta)

--- Html/JavaScript/Script.js
+++ Html/JavaScript/Script.js
@@ -1,6 +1,6 @@
 var lista = document.getElementById('lista');
 var pag = document.getElementById('page');
-var nav = document.getAnimations('nav');
+var nav = document.getElementById('nav');
 function sumir(lista,pag,nav) {
     if (lista.classList.contains("lista")) {
       lista.classList.remove("lista");
@@ -10,4 +10,9 @@ function sumir(lista,pag,nav) {
     } else {
       lista.classList.remove("oculto");
       lista.classList.add("lista");
+      pag.style.width = "85%";
+      nav.style.width = "100%";
+      lista.style.transition = "0.5s";
+      pag.style.transition = "0.5s";
+      
     }}
