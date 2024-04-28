a3a0833985b0cf568660898466b4f29bd5f349ca
hey0zenitsu <bd62593@gmail.com>
2024-04-28 13:12:28 -0300

    correção do corpo

--- Html/JavaScript/Script.js
+++ Html/JavaScript/Script.js
@@ -17,18 +17,4 @@ function sumir(lista,pag,nav) {
       pag.style.transition = "0.5s";
       
     }}
-    function sumir1(lista,pag,nav,list1) {
-      if (list1.classList.contains("list1")) {
-        list1.classList.remove("list1");
-        list1.classList.add("oculto");
-        pag.style.width = "100%";
-        nav.style.width = "100%";
-      } else {
-        list1.classList.remove("oculto1");
-        list1.classList.add("list1");
-        pag.style.width = "85%";
-        nav.style.width = "100%";
-        list1.style.transition = "0.5s";
-        pag.style.transition = "0.5s";
-        
-      }}
+   
