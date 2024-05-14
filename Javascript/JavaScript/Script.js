1f7c1dedb4cf3fc5754f27aaa3da9439485aa3f5
hey0zenitsu <bd62593@gmail.com>
2024-05-14 08:43:00 -0300

    add itens js

---
+++ Javascript/JavaScript/Script.js
@@ -0,0 +1,19 @@
+var lista = document.getElementById('lista');
+var pag = document.getElementById('page');
+var nav = document.getElementById('nav');
+var list1 = document.getElementById('list1');
+function sumir(lista,pag,nav) {
+    if (lista.classList.contains("lista")) {
+      lista.classList.remove("lista");
+      lista.classList.add("oculto");
+      pag.style.width = "100%";
+      nav.style.width = "100%";
+    } else {
+      lista.classList.remove("oculto");
+      lista.classList.add("lista");
+      pag.style.width = "85%";
+      nav.style.width = "100%";
+      lista.style.transition = "0.3s";
+      pag.style.transition = "0.3s";
+    }}
+   
