48cd71c330bea52b958825c4626acc6f8526ccae
hey0zenitsu <bd62593@gmail.com>
2024-07-26 23:06:19 -0300

    Arrumando animação do botão de lista

--- Html/JavaScript/Script.js
+++ Html/JavaScript/Script.js
@@ -2,12 +2,15 @@ var lista = document.getElementById('lista');
 var pag = document.getElementById('page');
 var nav = document.getElementById('nav');
 var list1 = document.getElementById('list1');
-function sumir(lista,pag,nav) {
+var btn = document.getElementById('btn_menu');
+function sumir(lista,pag,nav,btn) {
     if (lista.classList.contains("lista")) {
       lista.classList.remove("lista");
       lista.classList.add("oculto");
       pag.style.width = "100%";
       nav.style.width = "100%";
+      btn.style.transition = "0.5s"
+      btn.style.transform = "rotateZ(360deg)";
     } else {
       lista.classList.remove("oculto");
       lista.classList.add("lista");
@@ -15,5 +18,7 @@ function sumir(lista,pag,nav) {
       nav.style.width = "100%";
       lista.style.transition = "0.3s";
       pag.style.transition = "0.3s";
+      btn.style.transition = "0.5s"
+      btn.style.transform = "rotateZ(180deg)";
     }}
    
